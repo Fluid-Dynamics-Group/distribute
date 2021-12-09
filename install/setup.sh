03d1127fecb1a9d2b2a756a5baa92c58c51a653d
VanillaBrooks <brooks@karlik.org>
2021-12-08 18:32:01 -0800

    better install experience by updating the systemd file

--- install/setup.sh
+++ install/setup.sh
@@ -1,2 +1,13 @@
+git config --global credential.helper store
+git clone https://github.com/Fluid-Dynamics-Group/matrix-notify
+git clone https://github.com/Fluid-Dynamics-Group/distribute
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -- -y
+
 mkdir /home/distribute/data
 mkdir /home/distribute/logs
+
+cd distribute
+source $HOME/.cargo/env
+
+cargo install --path .
