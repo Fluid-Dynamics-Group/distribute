5a233687d429e3e7c4bda4262da79a10cc2fa5ee
VanillaBrooks <brooks@karlik.org>
2021-12-24 17:56:51 -0800

    setup file automatically install paraview headless

--- install/setup-root.sh
+++ install/setup-root.sh
@@ -1,1 +1,9 @@
+# download paraview 5.9.1 headless
+wget --output-document paraview59headless.tar.gz "https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v5.9&type=binary&os=Linux&downloadFile=ParaView-5.9.1-osmesa-MPI-Linux-Python3.8-64bit.tar.gz"
+tar -xvzf paraview59headless.tar.gz -C paraview59headless
+chmod -R 755 paraview59headless
+sudo mv paraview59headless /opt
+
+# now, the path reference in the compute service should be correct
+
 sudo cp distribute-compute.service /etc/systemd/system/
