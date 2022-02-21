7c9e2523147394d343172b64713af5a66971d5f0
VanillaBrooks <brooks@karlik.org>
2022-02-20 20:27:41 -0800

    updating based on git branch

--- install/update.sh
+++ install/update.sh
@@ -1,5 +1,7 @@
 # update the `distribute` binary
 # this script must be run from within the `distribute` 
 # repository
-git pull && \
-cargo install --path .
+RELEASE="release-0.8.0"
+git fetch -a
+git pull checkout $RELEASE
+cargo install --path ..
