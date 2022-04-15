11dc011bae9a5f4a4fd8e6aca8989c03699aa88f
VanillaBrooks <brooks@karlik.org>
2022-04-15 16:48:23 -0700

    update.sh goes to 0.9.0

--- install/update.sh
+++ install/update.sh
@@ -1,8 +1,8 @@
 # update the `distribute` binary
 # this script must be run from within the `distribute` 
 # repository
-RELEASE="release-0.8.0"
+RELEASE="release-0.9.0"
 git fetch -a &&\
-git checkout $RELEASE &&\
+	git checkout $RELEASE &&\
 git pull && \
-cargo install --path ..
+	cargo install --path ..
