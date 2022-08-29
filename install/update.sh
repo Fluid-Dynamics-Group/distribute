6a2bd129b13dc6c7c768d532b3482c3746fc1c42
VanillaBrooks <brooks@karlik.org>
2022-08-29 10:01:54 -0700

    update update.sh script

--- install/update.sh
+++ install/update.sh
@@ -1,8 +1,14 @@
-# update the `distribute` binary
-# this script must be run from within the `distribute` 
-# repository
-RELEASE="release-0.8.0"
-git fetch -a &&\
-git checkout $RELEASE &&\
-git pull && \
-cargo install --path ..
+# after sudo su distribute
+
+cd ~/distribute
+
+# for fish shell
+set VERSION "0.11.1"
+
+git fetch -a
+git checkout release-$VERSION
+git pull
+
+cargo install --path . --locked
+
+systemctl restart distribute-compute
