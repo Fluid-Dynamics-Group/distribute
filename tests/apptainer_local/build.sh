23ead66c8d279e1cfd80c0c966e982bc2a9bd618
VanillaBrooks <brooks@karlik.org>
2023-10-18 18:18:51 -0700

    crate feature for running apptainer tests

--- tests/apptainer_local/build.sh
+++ tests/apptainer_local/build.sh
@@ -1,3 +1,13 @@
+#! /usr/bin/bash
+
+# this script should be executed in its directory!
+# warning: this will create a folder at ~/apptainer/ and use it as a temporary directory
+# for building the container. The reasoning for this is detailed in the apptainer
+# documentation, but it it boils down to `/tmp` on linux being backed by an in-memory filesystem
+# that apptainer will often exceed: https://apptainer.org/docs/user/main/build_env.html#temporary-folders
+#
+# this folder is safe to remove once the build operation is complete
+
 OUT_SIF="apptainer_local.sif"
 rm $OUT_SIF
 
